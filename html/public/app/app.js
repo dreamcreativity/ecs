dc1f63723c3aeb9fa014af9d525df9053953ba4a
skylon88 <chengsun@Chengs-MacBook-Pro.local>
2015-02-16 14:30:06 -0500

    new

---
+++ html/public/app/app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+angular.module('EscApp', [
+    	'ngRoute',
+    	'controllers.staff',
+    	'services.staff'
+	])
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/login', {
+    	templateUrl: 'views/login.html',
+    	controller: 'LoginController'
+    });
+
+    $routeProvider.when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardController'
+    });
+
+    $routeProvider.when('/staffs', {
+        templateUrl: 'views/staff/staff1.html',
+        controller: 'StaffController'
+    });
+
+    $routeProvider.when('/staffs/create', {
+        templateUrl: 'views/staff/create.html',
+        controller: 'StaffController'
+    });
+
+
+
+    $routeProvider.otherwise({redirectTo: '/'});
+}])
+
+
+.controller('pageCtrl', function($rootScope, $location) {
+      if($location.path() === '/login'){
+        $rootScope.show_partial_view =false;
+      }
+      else {
+        $rootScope.show_partial_view = true;
+      }
+})
+
+.factory('authInterceptor', function ($rootScope, $q, $window) {
+  return {
+    request: function (config) {
+      config.headers = config.headers || {};
+      if ($window.sessionStorage.token) {
+        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+      }
+      return config;
+    },
+    response: function (response) {
+      if (response.status === 401) {
+        // handle the case where the user is not authenticated
+      }
+      if (response.status === 403) {
+        console.log('please log in ');
+      }
+      return response || $q.when(response);
+    }
+  };
+})
+
+.config(function ($httpProvider) {
+  //$httpProvider.interceptors.push('authInterceptor');
+});
+
---
+++ html/public/app/controllers/controllers.js
@@ -0,0 +1,63 @@
+'use strict'
+
+var app = angular.module('controllers.staff',[]);
+	
+
+app.controller('LoginController',function LoginController($rootScope,$scope,$http,$window,$location){
+	if($rootScope.show_partial_view == true){
+     	$window.location.reload()
+		 }
+	//Define login function 
+	$scope.login = function() {
+		$http.post('http://localhost:3000/api/staffs/login',$scope.staff)
+		.success(function(data,status,headers,config){
+			if(data.status === "ok"){
+				sessionStorage.setItem('token',data.data);
+				$location.path('/dashboard');
+			}
+			else {
+				console.log('login fail');
+			}
+		})
+		.error(function(data,status,headers,config){
+			console.log('error : ' + data );
+		});
+	};
+
+	//Log out 
+	$scope.logout = function(){
+		var token = sessionStorage.token;
+ 		var tokenPayload = jwtHelper.decodeToken(token);
+		delete sessionStorage.token;
+		$location.path('/login');
+	};
+});
+
+
+app.controller('DashboardController',function StaffController($rootScope,$http,$window,Staffs){
+	if($rootScope.show_partial_view == false){
+     	$window.location.reload()
+		 }
+});
+
+app.controller('StaffController',function StaffController($scope,$http,Staffs){
+	 var token = sessionStorage.token;
+	 $scope.staffs = getAllStaffs();
+	 $scope.create = function(){
+	 	Staffs.save($scope.staff,function(){
+ 			console.log('save success!');
+ 		})
+	 }
+
+ 	function getAllStaffs(){
+ 		return Staffs.query();
+ 	}
+
+
+ 	function getStaff(){
+
+ 	}
+})
+
+
+
