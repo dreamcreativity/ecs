65afc9ba8e30332916c31ffb8f19ae03d5a79dff
minxianzhou <loveandyzhou@Andys-MacBook-Pro.local>
2015-02-21 13:35:28 -0500

    ad ng folder

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'html')));
-//app.use(express.static(path.join(__dirname, 'static')));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
