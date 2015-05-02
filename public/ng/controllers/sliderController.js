2d9df2e8e839b48b833b5fd0a21d37b77044021a
skylon88 <chengsun@Chengs-MacBook-Pro.local>
2015-05-02 14:40:33 -0400

    merge

--- public/ng/controllers/sliderController.js
+++ public/ng/controllers/sliderController.js
@@ -2,7 +2,6 @@
 
 angular.module('AdminApp',['ngResource','esc.resources','esc.filters','esc.directives','esc.auth'])
 
-
 .controller('SliderController', function($rootScope,$scope,$location,$http,$window,Sliders,Medias){
 
 	if ( typeof url_params === "undefined" || typeof url_params.id === "undefined"){
@@ -68,37 +67,102 @@ angular.module('AdminApp',['ngResource','esc.resources','esc.filters','esc.direc
 				ShowGritterCenter('System Notification','Slider can not be created..' +  result.messages);
 				console.log(result.messages);
 			}
+.controller('SliderController', ['ngResource', function($rootScope,$scope){
+    console.log('here');
+	// if ( typeof url_params === "undefined" || typeof url_params.id === "undefined"){
+	// 	$scope.slider = {
+	// 		heading: '',
+	// 		sub_heading : '',
+	// 		color: '#ffffff',
+	// 		direction : 'bottom',
+	// 		position : 'CENTER',
+	// 		resource : null
+	// 	};
+	// }else{
+	// 	$scope.slider = Sliders.get(url_params, function(){
+	// 		$scope.slider = $scope.slider.data;	
+	// 	});
+	// }
+
+	// $scope.list = getAllSliders();
+	// $scope.media_list = getAllSliderMedia();
+
+
+
+	// $scope.colors = [
+	// 	{ name: 'Grey', code: '#cccccc'},
+	// 	{ name: 'White', code: '#ffffff'},
+	// 	{ name: 'Black', code: '#000000'},
+	// 	{ name: 'Red', code: '#f00'}
+	// ];
+
+	// $scope.directionOption = [ 
+	// 	{ class: 'fadeInLeft', label :'Left to Right' },
+	// 	{ class: 'fadeInRight', label : 'Right to Left' },
+	// 	{ class: 'fadeInDown', label : 'Top to Bottom' },
+	// 	{ class: 'fadeInUp', label :'Bottom to Top' }
+	// ];
+
+
+	// $scope.rgb2hex = function (rgb) {
+	// 	rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+	// 	function hex(x) {
+	// 	    return ("0" + parseInt(x).toString(16)).slice(-2);
+	// 	}
+	// 	return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+	// }
+
+	// function getAllSliders(){
+	// 	var result =  Sliders.query();
+	// 	//console.log(result);
+	// 	return result;
+	// }
+
+
+	// function getAllSliderMedia(){
+	// 	return Medias.query({'target': 'Slider'});
+	// }
+
+	// $scope.create = function(){
+	// 	Sliders.create($scope.slider,function(result){
+
+	// 		if(result.status == 'ok'){
+	// 			location = '/admin/slider/edit/'+ result.data._id;
+	// 		}else{
+	// 			ShowGritterCenter('System Notification','Slider can not be created..' +  result.messages);
+	// 			console.log(result.messages);
+	// 		}
 				
-		});
-	}
+	// 	});
+	// }
 
-	$scope.update = function(){
-		Sliders.update($scope.slider,function(result){
-				if(result.status == 'ok'){
-					ShowGritterCenter('System Notification','Slider has been updated');
-				}else{
-					ShowGritterCenter('System Notification','Slider updated fail');
-				}
-		});
-	}
+	// $scope.update = function(){
+	// 	Sliders.update($scope.slider,function(result){
+	// 			if(result.status == 'ok'){
+	// 				ShowGritterCenter('System Notification','Slider has been updated');
+	// 			}else{
+	// 				ShowGritterCenter('System Notification','Slider updated fail');
+	// 			}
+	// 	});
+	// }
 
-	$scope.delete = function(){
-		console.log('delete slider');
-		Sliders.delete(url_params,function(result){
-				if(result.status == 'ok'){
-					window.location = '/admin/slider/all';
-				}else{
-					ShowGritterCenter('System Notification','Slider can not being delete.');
-				}
-		});
-	}
+	// $scope.delete = function(){
+	// 	console.log('delete slider');
+	// 	Sliders.delete(url_params,function(result){
+	// 			if(result.status == 'ok'){
+	// 				window.location = '/admin/slider/all';
+	// 			}else{
+	// 				ShowGritterCenter('System Notification','Slider can not being delete.');
+	// 			}
+	// 	});
+	// }
 
 
-	$scope.selectResource = function(mediaResource){
-		console.log('click');
-		$scope.slider.resource = mediaResource;
+	// $scope.selectResource = function(mediaResource){
+	// 	console.log('click');
+	// 	$scope.slider.resource = mediaResource;
 
-	}
+	// }
 
 })
 .factory('Sliders',['$resource',
@@ -111,6 +175,7 @@ angular.module('AdminApp',['ngResource','esc.resources','esc.filters','esc.direc
 			update : { method : 'PUT', params: {id:'@_id'}}
 	});
 }])
+
 .directive('positionPicker', function() {
   return {
   	link: function (scope, element, attrs) {
@@ -156,12 +221,49 @@ angular.module('AdminApp',['ngResource','esc.resources','esc.filters','esc.direc
 });
 
 
+// .directive('positionPicker', function() {
+//   return {
+//   	link: function (scope, element, attrs) {
+//             element.on('click', function () {
+//                 scope.slider.position = element.html();
+//        			scope.$apply();
+//        			$('.slider-position .tap').removeClass('select');
+//        			element.parent().addClass('select');
+//             });
+//         }
+//   };
+// })
 
+// .directive('colorPicker', function() {
+//   return {
+//   	link: function (scope, element, attrs) {
+//             element.on('click', function () {
+//                 scope.$parent.slider.color = scope.rgb2hex(element.css('background-color'));
+//        			scope.$apply();
+//             });
+//         }
+//   };
+// })
 
+// .directive('resourcePicker', function() {
+//   return {
+//   	link: function (scope, element, attrs) {
+//             element.on('click', function () {
 
+            	
+//             	var newMedia = scope.media;
+//             	delete newMedia.$$hashKey;
+//             	console.log(newMedia);
+//             	//scope.$parent.slider.resource = scope.media;
+//             	scope.$parent.slider.resource = newMedia._id;
+//             	scope.$parent.slider.media = scope.media;
 
+//             	scope.$parent.$apply();
 
-
+//             });
+//         }
+//   };
+// })
 
 // .factory('Sliders',['$resource',
 // 	function($resource){
