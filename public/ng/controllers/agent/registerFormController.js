a7b7e877f940b9a61ec81c8b25b741536341dafb
skylon88 <stiron88@gmail.com>
2015-09-10 23:57:50 -0400

    add course

--- public/ng/controllers/agent/registerFormController.js
+++ public/ng/controllers/agent/registerFormController.js
@@ -1,8 +1,56 @@
 'use strict';
 angular.module('AgentApp')
 
-.controller('StudentRegister', function StudentRegister($rootScope, $scope, $http, Registrations, $window){
-	loading();
+.controller('StudentRegister', function StudentRegister($rootScope, $scope, $http, Registrations,Courses, $window){
+	$scope.courseList =[];
+	$scope.displayCourses = [];
+	var today = new Date();
+	$scope.availableYears = [
+	today.getFullYear(),
+	today.getFullYear()+1,
+	today.getFullYear()+2
+	];
+
+	$scope.$watch(
+		'courseList',
+		function(newValue,oldValue) {
+			$scope.displayCourses = [];
+			var total = 0;
+
+			angular.forEach($scope.courseList, function(course, key) {
+				
+				if(course.selectDuration.week > 0){
+					total += course.selectDuration.price;
+					var courseStartDate = new Date(course.startDate);
+					var courseEndDate = new Date(courseStartDate.valueOf());
+					courseEndDate.setDate( courseEndDate.getDate() + 7 * course.selectDuration.week);
+
+					$scope.displayCourses.push({
+						title: course.title,
+						startDate: course.startDate,
+						endDate: courseEndDate.toJSON(),
+						duration: course.selectDuration.week
+					});			
+				}
+
+
+			}, function(){
+
+			});
+			$scope.calculatedTotal = total;
+			console.log(total);
+
+		}, 
+		true
+		);
+
+	Registrations.query(function(result){
+		$scope.registrations = result;
+	});
+
+	Courses.getSimpleList(function(data){
+		$scope.courses = data.data;
+	});
 
 	$scope.register = function(isValid){
 		Registrations.save($scope.student, function(result){
@@ -10,11 +58,31 @@ angular.module('AgentApp')
 		});
 	}
 
-	function loading() {
-		Registrations.query(function(result){
-			$scope.registrations = result;
-			console.log("loaded registrations");
-		});
+	$scope.addCourse = function(course){
+		Courses.getCourstStartDateList({id:course._id, year:$scope.availableYears[0]}, function(data){
+			$scope.courseList.push({
+				id : course._id,
+				title: course.title,
+				selectDuration: { _id: '', title:'Select a Duration', price: 0, week : 0},
+				startDate: data.data[0],
+				durations: course.durations,
+				startDates: data.data,
+			});
+			closeAllSelectList();
+			setTimeout(function(){
+				initTitleBox();	
+			},50);
+		})
+	}
+
+	$scope.changeCourse = function(currentCourse, targetCourse){
+
+		currentCourse.id = targetCourse._id;
+		currentCourse.title = targetCourse.title;
+		currentCourse.selectDuration =  { _id: '', title:'Select a Duration', price: 0, week : 0},
+		currentCourse.durations = targetCourse.durations;
+		closeAllSelectList();
+
 	}
 })
 
