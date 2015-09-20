14fd41de6ef87e1f3d8d127cf4cb294b61fb9644
skylon88 <chengsun@Chengs-MacBook-Pro.local>
2015-09-20 16:45:52 -0400

    load template

--- constants.js
+++ constants.js
@@ -28,7 +28,33 @@ module.exports = {
 
 	// template seaction
 
-	// RegistrationTemplateVars = [
-	// 	'ProgramList' : 'ProgramList'
-	// ]
+	RegistrationTemplateVars : {
+		firstname : null,
+		lastname : null,
+		gender : null,
+		birthday : null,
+		age : null,
+		citizenship : null,
+		address : null,
+		city : null,
+		province : null,
+		country : null,
+		isHomeCountryAddress : null,
+		telephone : null,
+		fax : null,
+		email : null,
+		emergency : null,
+		englishLevel : null,
+		TOEFL : null,
+		IELTS : null,
+		healthInsuranceEndDate : null,
+		healthInsuranceStartingDate : null,
+		commissionRate : null,
+		pomotionRate : null
+	}
 }
+
+
+
+
+
