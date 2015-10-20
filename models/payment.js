c8352bc6a17635d447ac39bda591b429e1ea48ae
minxianzhou <loveandyzhou@Andys-MacBook-Pro.local>
2015-10-20 00:24:18 -0400

    update payment model

--- models/payment.js
+++ models/payment.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose');
 
 var PaymentSchema = new mongoose.Schema({
-	paymentType: {type: String, default: 'Payment'},
+	paymentType: {type: String},
 	paymentMethod : {type: String},
 	created : {type : Date},
 	paymentDate : {type : Date},
 	amount : {type : Number, min: 0 },
 	createBy: { type: Schema.Types.ObjectId, ref: 'Staff'},
+	isVoid : {type : Boolean, default: false }
 });
 
 var Payment = mongoose.model('Payment', PaymentSchema);
