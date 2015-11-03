fb319ee8ec0b0992add95d52f62f3776ffce261c
minxianzhou <minxianzhou@gmail.com>
2015-11-03 10:59:02 -0500

    fix staff can not open issue

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://dreamcwc:Asdf_1234@ds031571.mongolab.com:31571/ecs');
-mongoose.connect('mongodb://159.203.5.244:27017/ecs');
+mongoose.connect('mongodb://159.203.5.244:27017/ecs-demo');
 module.exports = mongoose.connection;
