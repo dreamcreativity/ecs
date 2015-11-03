d075d041e410d176fece99a8d612774999c22c94
minxianzhou <loveandyzhou@Andys-MacBook-Pro.local>
2015-11-02 23:54:47 -0500

    d

--- public/ng/lib/auth.js
+++ public/ng/lib/auth.js
@@ -2,16 +2,7 @@
 
 'use strict';
 
-var app = angular.module('esc.auth', [])
-
-.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1');
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-})
+var app = angular.module('esc.auth', []);
 
 
 app.factory('authInterceptor', function ($rootScope, $q, $window) {
@@ -24,8 +15,9 @@ app.factory('authInterceptor', function ($rootScope, $q, $window) {
   		if ($window.sessionStorage.token) {
 
 
-  			config.headers.api_token = $window.sessionStorage.token ;
-  	    console.log(config.headers.api_token );
+  			config.headers['api_token'] = $window.sessionStorage.token ;
+  	    console.log(config.headers );
+        //debugger;
   		}
   		return config;
     },
