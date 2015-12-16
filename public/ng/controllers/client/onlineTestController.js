f2f7ac0f403894b2c678392d67c84c6e2706d629
minxianzhou <minxianzhou@gmail.com>
2015-12-16 16:55:20 -0500

    online test ag update

--- public/ng/controllers/client/onlineTestController.js
+++ public/ng/controllers/client/onlineTestController.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('ClientApp')
-.controller('OnlineTestCtrl',function RegisterCtrl($rootScope,$scope,Constants,OnlineTest,$window){
+.controller('OnlineTestCtrl',function RegisterCtrl($rootScope,$scope,$interval,Constants,OnlineTest,$window){
 	
 	$scope.firstName = '';
 	$scope.lastName = '';
@@ -14,6 +14,11 @@ angular.module('ClientApp')
 	$scope.submited = false;
 	$scope.index = 1;
 
+
+	// time limit for test
+	var timeLimit = 60 * 30;
+	$scope.time = timeLimit;
+
 	Constants.get({name: 'Country'}, function(result){
 		$scope.countries = result.data;		
 	});
@@ -22,16 +27,31 @@ angular.module('ClientApp')
 	$scope.start = function(){
 		
 		OnlineTest.getTestQuestions({}, function(result){
-
+			$scope.time = timeLimit;
 			$scope.index = 0;
 			$scope.questions = result.data;
 			$scope.started = true;
 			$scope.done = false;
 			$scope.submited = false;
 			$scope.answers = {}; 
+
+			$scope.timeStart();
 			console.log($scope.questions);
 		});
 	};
+
+	$scope.timeStart = function(){
+		$scope.stopTime = $interval(function(){
+			$scope.time--;
+			if($scope.time <= 0){
+				$interval.cancel($scope.stopTime);
+
+				// do some handling when times up
+
+			}
+		}, 1000);
+	}
+
 
 	$scope.next = function(){
 
@@ -59,13 +79,6 @@ angular.module('ClientApp')
 
 	$scope.testFinish = function(){
 		$scope.done = true;
-
-		// OnlineTest.getNewRecord({}, function(result){
-		// 	console.log(result);
-		// });
-		
-		
-	
 	};
 
 	$scope.submitTest = function(){
@@ -148,4 +161,29 @@ angular.module('ClientApp')
 
 
 
+})
+
+.filter('toCountTime', function() {
+  return function(input) {
+    // input = input || '';
+    // var out = "";
+    // for (var i = 0; i < input.length; i++) {
+    //   out = input.charAt(i) + out;
+    // }
+    var min = Math.floor(input/60);
+    var second = input % 60;
+    var min_str = '';
+    var second_str = '';
+    if(min < 10)
+    	min_str = '0'+ min;
+    else
+    	min_str = ''+ min;
+    if(second < 10)
+    	second_str = '0'+ second;
+    else
+    	second_str = ''+ second;
+
+    var output = min_str + ' : ' + second_str;
+    return output;
+  };
 });
