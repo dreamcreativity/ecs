38318745bf50631d937aab9e49848dd6cc229bab
Minxianzhou <minxianzhou@gmail.com>
2016-07-25 00:52:33 -0400

    make slider subtitle can be optional

--- models/slider.js
+++ models/slider.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var SliderSchema = new mongoose.Schema({
 	heading: {type: String, required: true },
-	sub_heading: {type: String, required: true },
+	sub_heading: {type: String, required: false },
 	color: {type:String, required: true, default: '#bbb'},
 	direction: { type: String, required: true, default: 'bottom' },
 	resource: { type: Schema.Types.ObjectId, ref: 'Media', default: null },
