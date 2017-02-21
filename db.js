5f5b2da4eb1c00c4538f9f1bb45360532e56745a
Minxianzhou <minxianzhou@gmail.com>
2017-02-20 22:08:44 -0500

    update

--- db.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose');
-//mongoose.connect('mongodb://159.203.5.244:27017/ecs-demo');
-mongoose.connect('mongodb://159.203.5.244:27017/ecs');
-module.exports = mongoose.connection;
