63db58a6ac511659bc8a38c6caeffa6e4fcb96bd
Minxianzhou <minxianzhou@gmail.com>
2017-03-04 12:50:30 -0500

    update

--- public/plugin/simditor/js/doc_directive.js
+++ public/plugin/simditor/js/doc_directive.js
@@ -16,7 +16,7 @@
         scope.simditor = new Simditor({
           textarea: element.children()[0],
           pasteImage: true,
-          toolbar: ['title', 'bold', 'italic', 'underline', 'strikethrough', '|', 'ol', 'ul', 'blockquote', 'code', 'table', '|', 'link', 'image', 'hr', '|', 'indent', 'outdent'],
+          toolbar: ['title', 'bold', 'italic', 'underline', 'strikethrough', '|', 'ol', 'ul','code', 'table', '|', 'link', 'image', 'hr', '|', 'indent', 'outdent' ],
           defaultImage: 'assets/images/image.png',
           upload: location.search === '?upload' ? {
             url: '/upload'
